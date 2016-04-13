bef462281164697980c1dfc83a670e3f71002e11
clpong <clpong@hotmail.com>
2016-04-13 14:32:37 +0800

    updated server.R

--- server.R
+++ server.R
@@ -1,7 +1,10 @@
 library(shiny)
 shinyServer(
   function(input, output){
-     output$MonthlyRepayment <- renderText({ CalculatCarRepayment(input$Principal, input$InterestRate, input$Tenure) })   
+     output$Principal <- renderText({input$Principal})
+     output$MonthlyRepayment <- renderText({ CalculatCarRepayment(input$Principal, input$InterestRate, input$Tenure) })
+     output$TotalPayment <- renderText({ CalculateTotalPayment(input$Principal, input$InterestRate, input$Tenure) })
+     output$TotalInterestPay <- renderText({ CalculateCarInterest(input$Principal, input$InterestRate, input$Tenure) })
   }
 )
 
@@ -13,3 +16,21 @@ CalculatCarRepayment <- function (Principal, InterestRate, Tenure)
   return(round(result, digits = 2))
 }
 
+CalculateTotalPayment <- function (Principal, InterestRate, Tenure)
+{
+  IR <- InterestRate/100
+  TenureInMonth <- Tenure * 12 
+  MonthlyPayment <- (Principal*(IR/12))/(1 - ( 1 + IR / 12 )^-TenureInMonth)
+  result <- MonthlyPayment * TenureInMonth
+  return(round(result, digits = 0))
+}
+
+CalculateCarInterest <- function (Principal, InterestRate, Tenure)
+{
+  IR <- InterestRate/100
+  TenureInMonth <- Tenure * 12 
+  MonthlyPayment <- (Principal*(IR/12))/(1 - ( 1 + IR / 12 )^-TenureInMonth)
+  TotalPayment <- MonthlyPayment * TenureInMonth
+  result <- TotalPayment - Principal
+  return(round(result, digits = 0))
+}
