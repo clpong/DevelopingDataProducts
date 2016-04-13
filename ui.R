e0d87aeee4530ce84637919e29a7a2c6da75dcf9
clpong <clpong@hotmail.com>
2016-04-13 14:32:46 +0800

    updated ui.R

--- ui.R
+++ ui.R
@@ -8,8 +8,15 @@ shinyUI(pageWithSidebar(
     
   ),
   mainPanel(
+    h3('Loan Amount (MYR)'),
+    strong(h4(textOutput("Principal"))),
     h3('Monthly Car Loan Repayment (MYR)'),
     strong(h4(textOutput("MonthlyRepayment"))),
+    h3('Total Payment (MYR)'),
+    strong(h4(textOutput("TotalPayment"))),
+    h3('Total Interest To Pay (MYR)'),
+    strong(h4(textOutput("TotalInterestPay"))),
+    
     br(),
     code("Instructions"),
     helpText("This application is for car purchaser to calculate monthly repayment base on bank given interest rate")
